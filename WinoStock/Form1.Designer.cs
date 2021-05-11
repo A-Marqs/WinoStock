2a845e7a6320fa5158eb2620085ce42f40729964
Kauã Morateli <kmelo249@gmail.com>
2021-05-11 10:13:13 -0300

    Criação Front Login

--- WinoStock/Form1.Designer.cs
+++ WinoStock/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WinoStock
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,175 @@ namespace WinoStock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblWinostock = new System.Windows.Forms.Label();
+            this.lblDev = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Purple;
+            this.panel1.Controls.Add(this.lblDev);
+            this.panel1.Controls.Add(this.lblWinostock);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-2, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinoStock.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(32, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 138);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblWinostock
+            // 
+            this.lblWinostock.AutoSize = true;
+            this.lblWinostock.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblWinostock.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblWinostock.Location = new System.Drawing.Point(57, 190);
+            this.lblWinostock.Name = "lblWinostock";
+            this.lblWinostock.Size = new System.Drawing.Size(163, 36);
+            this.lblWinostock.TabIndex = 1;
+            this.lblWinostock.Text = "WinoStock";
+            // 
+            // lblDev
+            // 
+            this.lblDev.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDev.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDev.Location = new System.Drawing.Point(0, 374);
+            this.lblDev.Name = "lblDev";
+            this.lblDev.Size = new System.Drawing.Size(118, 52);
+            this.lblDev.TabIndex = 2;
+            this.lblDev.Text = "Desenvolvido por Aldrin Marques e Kauã Morateli";
+            this.lblDev.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLogin.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lblLogin.Location = new System.Drawing.Point(286, 32);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(92, 36);
+            this.lblLogin.TabIndex = 2;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUser.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lblUser.Location = new System.Drawing.Point(286, 133);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(51, 17);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "Usuário";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSenha.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lblSenha.Location = new System.Drawing.Point(286, 224);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(45, 17);
+            this.lblSenha.TabIndex = 4;
+            this.lblSenha.Text = "Senha";
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.Color.Purple;
+            this.pnlLogin.Controls.Add(this.label1);
+            this.pnlLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pnlLogin.Location = new System.Drawing.Point(352, 320);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(186, 43);
+            this.pnlLogin.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(51, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Login";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Purple;
+            this.panel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel2.Location = new System.Drawing.Point(286, 177);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(203, 2);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Purple;
+            this.panel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel3.Location = new System.Drawing.Point(286, 269);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(203, 2);
+            this.panel3.TabIndex = 8;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(612, 425);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlLogin);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDev;
+        private System.Windows.Forms.Label lblWinostock;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
