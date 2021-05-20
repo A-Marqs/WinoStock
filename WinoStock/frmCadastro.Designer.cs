a403a5e69eaa440d2af0daccc17c71ae0d3e5f14
Kauã Morateli <kmelo249@gmail.com>
2021-05-20 10:52:57 -0300

    Modificações Front

--- WinoStock/frmCadastro.Designer.cs
+++ WinoStock/frmCadastro.Designer.cs
@@ -29,12 +29,229 @@ namespace WinoStock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbxTipo = new System.Windows.Forms.ComboBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.cbxLocal = new System.Windows.Forms.ComboBox();
+            this.lblLocal = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.lblUn = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxTipo
+            // 
+            this.cbxTipo.FormattingEnabled = true;
+            this.cbxTipo.Items.AddRange(new object[] {
+            "Periferico",
+            "Monitor",
+            "Computador",
+            "Peça",
+            "Material de Escritorio",
+            "Insumo"});
+            this.cbxTipo.Location = new System.Drawing.Point(12, 26);
+            this.cbxTipo.Name = "cbxTipo";
+            this.cbxTipo.Size = new System.Drawing.Size(121, 23);
+            this.cbxTipo.TabIndex = 0;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.BackColor = System.Drawing.Color.White;
+            this.lblTipo.ForeColor = System.Drawing.Color.Black;
+            this.lblTipo.Location = new System.Drawing.Point(12, 9);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(33, 15);
+            this.lblTipo.TabIndex = 1;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.ForeColor = System.Drawing.Color.Black;
+            this.lblId.Location = new System.Drawing.Point(13, 60);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(20, 15);
+            this.lblId.TabIndex = 3;
+            this.lblId.Text = "Id:";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.ForeColor = System.Drawing.Color.Black;
+            this.lblData.Location = new System.Drawing.Point(91, 61);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(93, 15);
+            this.lblData.TabIndex = 5;
+            this.lblData.Text = "Data de Entrada:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(92, 76);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // cbxLocal
+            // 
+            this.cbxLocal.FormattingEnabled = true;
+            this.cbxLocal.Items.AddRange(new object[] {
+            "Gaveteiro 1",
+            "Gaveteiro 2",
+            "Gaveteiro 3",
+            "Estoque",
+            "Armario Estoque"});
+            this.cbxLocal.Location = new System.Drawing.Point(229, 76);
+            this.cbxLocal.Name = "cbxLocal";
+            this.cbxLocal.Size = new System.Drawing.Size(121, 23);
+            this.cbxLocal.TabIndex = 7;
+            // 
+            // lblLocal
+            // 
+            this.lblLocal.AutoSize = true;
+            this.lblLocal.ForeColor = System.Drawing.Color.Black;
+            this.lblLocal.Location = new System.Drawing.Point(228, 61);
+            this.lblLocal.Name = "lblLocal";
+            this.lblLocal.Size = new System.Drawing.Size(38, 15);
+            this.lblLocal.TabIndex = 8;
+            this.lblLocal.Text = "Local:";
+            this.lblLocal.UseMnemonic = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 129);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 23);
+            this.textBox2.TabIndex = 9;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.BackColor = System.Drawing.Color.White;
+            this.lblDescricao.ForeColor = System.Drawing.Color.Black;
+            this.lblDescricao.Location = new System.Drawing.Point(13, 112);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(61, 15);
+            this.lblDescricao.TabIndex = 10;
+            this.lblDescricao.Text = "Descrição:";
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.BackColor = System.Drawing.Color.White;
+            this.lblQtd.ForeColor = System.Drawing.Color.Black;
+            this.lblQtd.Location = new System.Drawing.Point(312, 113);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(30, 15);
+            this.lblQtd.TabIndex = 11;
+            this.lblQtd.Text = "Qtd:";
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(311, 130);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(37, 23);
+            this.txtQtd.TabIndex = 12;
+            // 
+            // lblUn
+            // 
+            this.lblUn.AutoSize = true;
+            this.lblUn.BackColor = System.Drawing.Color.White;
+            this.lblUn.ForeColor = System.Drawing.Color.Black;
+            this.lblUn.Location = new System.Drawing.Point(256, 113);
+            this.lblUn.Name = "lblUn";
+            this.lblUn.Size = new System.Drawing.Size(35, 15);
+            this.lblUn.TabIndex = 13;
+            this.lblUn.Text = "Unid:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "UN",
+            "CX",
+            "PC",
+            "M",
+            "CM"});
+            this.comboBox1.Location = new System.Drawing.Point(257, 130);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(34, 23);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(257, 180);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(93, 40);
+            this.btnSalvar.TabIndex = 15;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // frmCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmCadastro";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(371, 233);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblUn);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblLocal);
+            this.Controls.Add(this.cbxLocal);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.cbxTipo);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCadastro";
+            this.Text = "r";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbxTipo;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ComboBox cbxLocal;
+        private System.Windows.Forms.Label lblLocal;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Label lblQtd;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Label lblUn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSalvar;
     }
 }
--- WinoStock/frmMenu.Designer.cs
+++ WinoStock/frmMenu.Designer.cs
@@ -31,12 +31,13 @@ namespace WinoStock
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblRelatorio = new System.Windows.Forms.Label();
+            this.lblAdm = new System.Windows.Forms.Label();
+            this.lblSair = new System.Windows.Forms.Label();
+            this.lblAtualizacao = new System.Windows.Forms.Label();
             this.lblConsulta = new System.Windows.Forms.Label();
             this.lblCadastro = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -45,12 +46,11 @@ namespace WinoStock
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -62,6 +62,17 @@ namespace WinoStock
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(915, 55);
             this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(858, 8);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
@@ -76,10 +87,10 @@ namespace WinoStock
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(53)))), ((int)(((byte)(64)))));
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblRelatorio);
+            this.panel2.Controls.Add(this.lblAdm);
+            this.panel2.Controls.Add(this.lblSair);
+            this.panel2.Controls.Add(this.lblAtualizacao);
             this.panel2.Controls.Add(this.lblConsulta);
             this.panel2.Controls.Add(this.lblCadastro);
             this.panel2.Location = new System.Drawing.Point(0, 54);
@@ -87,51 +98,51 @@ namespace WinoStock
             this.panel2.Size = new System.Drawing.Size(155, 438);
             this.panel2.TabIndex = 1;
             // 
-            // label4
+            // lblRelatorio
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label4.Location = new System.Drawing.Point(37, 181);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 21);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Relatórios";
-            this.label4.Visible = false;
+            this.lblRelatorio.AutoSize = true;
+            this.lblRelatorio.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRelatorio.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblRelatorio.Location = new System.Drawing.Point(37, 181);
+            this.lblRelatorio.Name = "lblRelatorio";
+            this.lblRelatorio.Size = new System.Drawing.Size(84, 21);
+            this.lblRelatorio.TabIndex = 8;
+            this.lblRelatorio.Text = "Relatórios";
+            this.lblRelatorio.Visible = false;
             // 
-            // label3
+            // lblAdm
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label3.Location = new System.Drawing.Point(21, 363);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 21);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Administrador";
-            this.label3.Visible = false;
+            this.lblAdm.AutoSize = true;
+            this.lblAdm.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAdm.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblAdm.Location = new System.Drawing.Point(21, 363);
+            this.lblAdm.Name = "lblAdm";
+            this.lblAdm.Size = new System.Drawing.Size(115, 21);
+            this.lblAdm.TabIndex = 7;
+            this.lblAdm.Text = "Administrador";
+            this.lblAdm.Visible = false;
             // 
-            // label2
+            // lblSair
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label2.Location = new System.Drawing.Point(58, 398);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 21);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Sair";
+            this.lblSair.AutoSize = true;
+            this.lblSair.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSair.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblSair.Location = new System.Drawing.Point(58, 398);
+            this.lblSair.Name = "lblSair";
+            this.lblSair.Size = new System.Drawing.Size(37, 21);
+            this.lblSair.TabIndex = 6;
+            this.lblSair.Text = "Sair";
             // 
-            // label1
+            // lblAtualizacao
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label1.Location = new System.Drawing.Point(35, 126);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 21);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Atualização";
+            this.lblAtualizacao.AutoSize = true;
+            this.lblAtualizacao.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAtualizacao.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblAtualizacao.Location = new System.Drawing.Point(35, 126);
+            this.lblAtualizacao.Name = "lblAtualizacao";
+            this.lblAtualizacao.Size = new System.Drawing.Size(93, 21);
+            this.lblAtualizacao.TabIndex = 5;
+            this.lblAtualizacao.Text = "Atualização";
             // 
             // lblConsulta
             // 
@@ -228,17 +239,6 @@ namespace WinoStock
             this.label5.Text = "Cadastro";
             this.label5.Visible = false;
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(858, 8);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(51, 35);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -252,12 +252,12 @@ namespace WinoStock
             this.Name = "frmMenu";
             this.Text = "Menu";
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -267,10 +267,10 @@ namespace WinoStock
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRelatorio;
+        private System.Windows.Forms.Label lblAdm;
+        private System.Windows.Forms.Label lblSair;
+        private System.Windows.Forms.Label lblAtualizacao;
         private System.Windows.Forms.Label lblConsulta;
         private System.Windows.Forms.Label lblCadastro;
         private System.Windows.Forms.Panel panel3;
